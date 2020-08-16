3b37c02f44c7b05b02db3886147a559434c2e48b
MS <msulek59@hotmail.com>
2020-08-16 21:25:51 +0200

    added Task model

--- pdca/board/models.py
+++ pdca/board/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    problem = models.TextField()
+    action = models.TextField()
+    date_notification = models.DateTimeField(default=timezone.now)
+    in_charge = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
