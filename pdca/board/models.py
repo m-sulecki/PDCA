bd5f834d0abf05900bd5f1213032f35682256d6c
MS <msulek59@hotmail.com>
2020-08-16 21:52:01 +0200

    started app users

--- pdca/board/models.py
+++ pdca/board/models.py
@@ -2,10 +2,20 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Task(models.Model):
+
+    HIGH = 3
+    MEDIUM = 2
+    SMALL = 1
+
+    LEVELS = (
+        (HIGH, "high"),
+        (MEDIUM, "medium"),
+        (SMALL, "small"),
+    )
     problem = models.TextField()
     action = models.TextField()
     date_notification = models.DateTimeField(default=timezone.now)
     in_charge = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
+    importance = models.IntegerField(choices=LEVELS, default=2)
